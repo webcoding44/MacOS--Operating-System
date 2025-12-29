de98796668868c4845bd5642c197329598f947f0
safiurahmanjami <webcoding44@gmail.com>
2025-12-30 04:18:46 +0430

    fix: correct filename casing for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-// import{ resolve , dirname } from 'path';
-// import { fileURLToPath } from 'url';
 
 
 
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // resolve : {
-  //   alias : {
-  //     "#components" : resolve(
-  //       dirname(fileURLToPath(import.meta.url)),
-  //       "src/components",
-  //     ),
-  //     "#store" : resolve (dirname(fileURLToPath(import.meta.url)), "src/store"),
-  //     "#hos" : resolve (dirname(fileURLToPath(import.meta.url)), "src/hoc"),
-  //     "#windows" : resolve (dirname (fileURLToPath(import.meta.url)), "src/windows"),
-  //     "#constants": resolve(dirname(fileURLToPath(import.meta.url)), "src/constants"),
-  //   },
-  // }
 })
